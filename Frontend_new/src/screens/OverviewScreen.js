b0376887295520a4791a7c2fd9b42af841a7d7b4
Vedant Pandey <Vedant799@gmail.com>
2024-11-03 23:22:57 +0100

    Update OverviewScreen.js

--- Frontend_new/src/screens/OverviewScreen.js
+++ Frontend_new/src/screens/OverviewScreen.js
@@ -4,12 +4,16 @@ import { View, Text, Button } from 'react-native';
 
 export default function OverviewScreen({ navigation }) {
   return (
-    <View>
-      <Text>Overview Screen</Text>
-      <Button
-        title="Go to Habit Screen"
-        onPress={() => navigation.navigate('Habit')}
-      />
-    </View>
+      <View>
+          <h1>Overview Screen</h1>
+          <h2>Calculate your Carbon Footprint</h2>
+          <a href="https://www.footprintcalculator.org/home/en">Footprint Calculator</a>
+
+
+          <Button
+              title="Go to Habit Screen"
+              onPress={() => navigation.navigate('Habit')}
+          />
+      </View>
   );
 }
