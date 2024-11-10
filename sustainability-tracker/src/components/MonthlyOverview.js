c100524cbedafac6daca35e3b39a12283b614d8a
Aarushi Prasad <aarushipr4@gmail.com>
2024-11-10 21:53:42 +0100

    updated calender grid
    first of month starts on right day
    all weekends red
    current day highlighted

--- sustainability-tracker/src/components/MonthlyOverview.js
+++ sustainability-tracker/src/components/MonthlyOverview.js
@@ -10,6 +10,7 @@ function MonthlyOverview({ onSelectDay }) {
 
   // Get the current date
   const currentDate = new Date();
+  const currentDay = currentDate.getDate();
   const currentMonth = currentDate.getMonth() + 1;  // getMonth() returns 0-11, so we add 1
   const currentYear = currentDate.getFullYear();  // Get the current year
 
@@ -17,10 +18,17 @@ function MonthlyOverview({ onSelectDay }) {
   const [month, setMonth] = useState(currentMonth);
   const [year, setYear] = useState(currentYear);
 
+  // Get the first day of the month (0 = Sunday, 1 = Monday, etc.)
+  const firstDayOfMonth = new Date(year, month - 1, 1).getDay() - 1;
+
   // Function to calculate the number of days in a month
   const daysInMonth = (month, year) => new Date(year, month, 0).getDate();  // Ensure month is 0-indexed
-  const days = Array.from({ length: daysInMonth(month, year) }, (_, i) => i + 1);
+  const days = Array.from({ length: firstDayOfMonth }, () => "").concat(
+    Array.from({ length: daysInMonth(month, year) }, (_, i) => i + 1));
 
+  console.log('Days in a month: ', daysInMonth);
+  console.log('first day: ', firstDayOfMonth);
+  
   // Format the month and year for display
   const formattedMonthYear = new Date(year, month - 1).toLocaleString('default', { month: 'long', year: 'numeric' });
 
@@ -28,13 +36,13 @@ function MonthlyOverview({ onSelectDay }) {
     <div className="monthly-overview-container">
       <h2>{formattedMonthYear}</h2>
       <div className="calendar-grid">
-        {days.map(day => (
+        {days.map((day, index) => (
           <div
-            key={day}
-            className="calendar-day"
-            onClick={() => onSelectDay(day)}
+            key={index}
+            className={`calendar-day ${day === currentDay && month === currentMonth && year === currentYear ? 'current-day' : ''}`}
+            onClick={() => day && onSelectDay(day)}
           >
-            <span>{day}</span>
+            {day || ""}
           </div>
         ))}
       </div>
